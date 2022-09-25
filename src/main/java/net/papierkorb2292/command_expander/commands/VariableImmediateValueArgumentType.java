471fb9c0596a82cf1a8c3ca15819ced96e5d1e85
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2022-09-25 13:46:27 +0200

    Added option to VariableImmediateValueArgumentType to expect parentheses around the immediate value, so it can be used as value in vanilla commands.

--- src/main/java/net/papierkorb2292/command_expander/commands/VariableImmediateValueArgumentType.java
+++ src/main/java/net/papierkorb2292/command_expander/commands/VariableImmediateValueArgumentType.java
@@ -17,9 +17,21 @@ import java.util.concurrent.CompletableFuture;
 public class VariableImmediateValueArgumentType implements ArgumentType<ImmediateValue> {
 
     private static final Collection<String> EXAMPLES = Arrays.asList("3", "\"I am a string!\"", "0x3", "5-2", "{ sin(a:my_var[\"2\" + \"292\"] - (int)b:my_other_var * -7), (1 + key(c:some_elses_var)) * 3.5f }");
+    private static final Collection<String> PARENTHESES_EXAMPLES = EXAMPLES.stream().map(example -> '(' + example + ')').toList();
+
+    private final boolean expectOuterParentheses;
+
+    public VariableImmediateValueArgumentType(boolean expectOuterParentheses) {
+
+        this.expectOuterParentheses = expectOuterParentheses;
+    }
 
     public static VariableImmediateValueArgumentType variableImmediateValue() {
-        return new VariableImmediateValueArgumentType();
+        return variableImmediateValue(false);
+    }
+
+    public static VariableImmediateValueArgumentType variableImmediateValue(boolean expectOuterParentheses) {
+        return new VariableImmediateValueArgumentType(expectOuterParentheses);
     }
 
     public static ImmediateValue getImmediateValue(CommandContext<ServerCommandSource> context, String name) {
@@ -28,6 +40,12 @@ public class VariableImmediateValueArgumentType implements ArgumentType<Immediat
 
     @Override
     public ImmediateValue parse(StringReader reader) throws CommandSyntaxException {
+        if(expectOuterParentheses) {
+            reader.expect('(');
+            ImmediateValue result = ImmediateValueCompiler.compile(reader);
+            reader.expect(')');
+            return result;
+        }
         return ImmediateValueCompiler.compile(reader);
     }
 
@@ -38,6 +56,6 @@ public class VariableImmediateValueArgumentType implements ArgumentType<Immediat
 
     @Override
     public Collection<String> getExamples() {
-        return EXAMPLES;
+        return expectOuterParentheses ? PARENTHESES_EXAMPLES : EXAMPLES;
     }
 }
